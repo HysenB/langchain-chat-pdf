c4150d62e5cf02a7665934882dac2a42221951ec
HysenB <hysi2000@live.com>
2024-07-14 16:20:23 +0200

    creating chunks

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import streamlit as st
-from PyPDF2 import PDFReader
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
 def main():
     load_dotenv()
     st.set_page_config(page_title='Ask your PDF')
@@ -9,7 +11,22 @@ def main():
     pdf = st.file_uploader('Upload your pdf', type="PDF")
     
     if pdf is not None:
-        pdf_reader = PDFReader(pdf)
-
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+            
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+        st.write(chunks)
+            
+            
+            
 if __name__ == '__main__':
     main()
